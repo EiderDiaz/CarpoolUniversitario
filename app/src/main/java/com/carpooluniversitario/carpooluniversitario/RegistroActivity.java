d7dc7586ddf687444733b1f64e5feafd97042b93
eider <eiderdiazmoraila@hotmail.com>
2018-04-23 21:42:24 -0700

    [ADD] el cascaron de mainactivity el cual el es formulario para publicar o encontrar viaje

--- app/src/main/java/com/carpooluniversitario/carpooluniversitario/RegistroActivity.java
+++ app/src/main/java/com/carpooluniversitario/carpooluniversitario/RegistroActivity.java
@@ -1,7 +1,10 @@
 package com.carpooluniversitario.carpooluniversitario;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class RegistroActivity extends AppCompatActivity {
 
@@ -10,6 +13,14 @@ public class RegistroActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registro);
 
-
+        Button button= findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(),MainActivity.class);
+                startActivity(intent);
+            }
+        });
     }
+
 }
