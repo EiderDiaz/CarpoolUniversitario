88114d6674ec8d7329b4317f51d45b7381aa5647
eider <eiderdiazmoraila@hotmail.com>
2018-05-07 18:39:02 -0700

    [UPDATE] implementado sariparr en el login

--- app/src/main/java/com/carpooluniversitario/carpooluniversitario/MainActivity.java
+++ app/src/main/java/com/carpooluniversitario/carpooluniversitario/MainActivity.java
@@ -1,7 +1,12 @@
 package com.carpooluniversitario.carpooluniversitario;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.facebook.AccessToken;
+
+import static android.content.Intent.FLAG_ACTIVITY_CLEAR_TASK;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        if (AccessToken.getCurrentAccessToken() != null) {
+            goLoginScreen();
+        }
+    }
+
+
+    private void goLoginScreen() {
+        Intent intent = new Intent(this, RegistroActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(intent);
     }
 }
--- app/src/main/java/com/carpooluniversitario/carpooluniversitario/RegistroActivity.java
+++ app/src/main/java/com/carpooluniversitario/carpooluniversitario/RegistroActivity.java
@@ -5,77 +5,107 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
+import com.facebook.AccessToken;
 import com.facebook.CallbackManager;
 import com.facebook.FacebookCallback;
 import com.facebook.FacebookException;
 import com.facebook.GraphRequest;
 import com.facebook.GraphResponse;
+import com.facebook.HttpMethod;
+import com.facebook.login.LoginManager;
 import com.facebook.login.LoginResult;
 import com.facebook.login.widget.LoginButton;
 import com.facebook.share.internal.ShareConstants;
+import com.mobsandgeeks.saripaar.ValidationError;
+import com.mobsandgeeks.saripaar.Validator;
+import com.mobsandgeeks.saripaar.annotation.NotEmpty;
 
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.util.List;
+
 import static android.content.Intent.FLAG_ACTIVITY_CLEAR_TASK;
 
-public class RegistroActivity extends AppCompatActivity {
+public class RegistroActivity extends AppCompatActivity implements Validator.ValidationListener{
 
     CallbackManager callbackManager;
     LoginButton loginButton;
+    @NotEmpty(message = "Esta campo es obligatorio")
+    EditText txt_origen,txt_destino,txt_numeroDeControl,txt_carrera,txt_semestre;
+    Validator validator;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registro);
         loginButton = (LoginButton) findViewById(R.id.login_button);
+        txt_origen = findViewById(R.id.txtorigen);
+        txt_destino = findViewById(R.id.txtdestino);
+        txt_numeroDeControl = findViewById(R.id.txtmatricula);
+        txt_carrera = findViewById(R.id.txtcarrera);
+        txt_semestre = findViewById(R.id.txtsemestre);
+        validator= new Validator(this);
+        validator.setValidationListener(this);
 
-        loginButton.setOnClickListener(new View.OnClickListener() {
+
+
+            loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
-                startActivity(intent);
-                callbackManager = CallbackManager.Factory.create();
-                loginButton.setReadPermissions("email","public_profile");
-                // Callback registration
-                loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
-                    @Override
-                    public void onSuccess(LoginResult loginResult) {
-                        String userid = loginResult.getAccessToken().getUserId();
-                        GraphRequest graphRequest = GraphRequest.newMeRequest(loginResult.getAccessToken(), new GraphRequest.GraphJSONObjectCallback() {
-                            @Override
-                            public void onCompleted(JSONObject object, GraphResponse response) {
-                               displayUserInfo(object);
+                IniciarSesionFacebook();
 
-                            }
-                        });
-                        Bundle bundle = new Bundle();
-                        bundle.putString(GraphRequest.FIELDS_PARAM, "first_name,last_name,email,id");
-                        graphRequest.setParameters(bundle);
-                        graphRequest.executeAsync();
-                        goMainScreen();
-                    }
-
-                    @Override
-                    public void onCancel() {
-                        Toast.makeText(getApplicationContext(), "sesion cancelada", Toast.LENGTH_SHORT).show();
-                    }
-
-                    @Override
-                    public void onError(FacebookException exception) {
-                        Toast.makeText(getApplicationContext(), "error: " + exception.getMessage(), Toast.LENGTH_SHORT).show();
-                    }
-                });
             }
         });
 
+
+
+
+
     }
 
-        private void goMainScreen() {
+    private void IniciarSesionFacebook() {
+        callbackManager = CallbackManager.Factory.create();
+
+        loginButton.setReadPermissions("email","public_profile");
+        // Callback registration
+        loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                String userid = loginResult.getAccessToken().getUserId();
+                GraphRequest graphRequest = GraphRequest.newMeRequest(loginResult.getAccessToken(), new GraphRequest.GraphJSONObjectCallback() {
+                    @Override
+                    public void onCompleted(JSONObject object, GraphResponse response) {
+                        displayUserInfo(object);
+
+                    }
+                });
+                Bundle bundle = new Bundle();
+                bundle.putString(GraphRequest.FIELDS_PARAM, "first_name,last_name,email,id");
+                graphRequest.setParameters(bundle);
+                graphRequest.executeAsync();
+                validator.validate();
+
+            }
+
+            @Override
+            public void onCancel() {
+                Toast.makeText(getApplicationContext(), "sesion cancelada", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onError(FacebookException exception) {
+                Toast.makeText(getApplicationContext(), "error: " + exception.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    private void goMainScreen() {
             Intent intent = new Intent(this, MainActivity.class);
-            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP|FLAG_ACTIVITY_CLEAR_TASK);
+            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
             startActivity(intent);
         }
 
@@ -94,11 +124,39 @@ public class RegistroActivity extends AppCompatActivity {
             }
             Toast.makeText(this, "nombre:" + firstname + "\napellido:" + lastname + "\nemail:" + email + "\nid:" + id, Toast.LENGTH_SHORT).show();
         }
-
+    public void disconnectFromFacebook(View view) {
+        if (AccessToken.getCurrentAccessToken() != null) {
+            new GraphRequest(AccessToken.getCurrentAccessToken(), "/me/permissions/", null, HttpMethod.DELETE, new GraphRequest.Callback() {
+                @Override
+                public void onCompleted(GraphResponse response) {
+                    LoginManager.getInstance().logOut();
+                }
+            }).executeAsync();
+        }
+    }
         protected void onActivityResult(int requestCode, int resultCode, Intent data) {
             super.onActivityResult(requestCode, resultCode, data);
             this.callbackManager.onActivityResult(requestCode, resultCode, data);
         }
 
 
+    @Override
+    public void onValidationSucceeded() {
+        goMainScreen();
+
+    }
+
+    @Override
+    public void onValidationFailed(List<ValidationError> errors) {
+        for (ValidationError error : errors) {
+            View view = error.getView();
+            String message = error.getCollatedErrorMessage(this);
+            // Display error messages ;)
+            if (view instanceof EditText) {
+                ((EditText) view).setError(message);
+            } else {
+                Toast.makeText(this, message, Toast.LENGTH_LONG).show();
+            }
+        }
+    }
 }
