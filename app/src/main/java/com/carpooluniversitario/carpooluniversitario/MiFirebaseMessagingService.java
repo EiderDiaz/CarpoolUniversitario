22e2262fdec0fbee8d8c04818b0e14b51ecf580e
eider <eiderdiazmoraila@hotmail.com>
2018-05-22 21:30:16 -0700

    [update] listos las notificaciones con estilo
    [falta]
    -registrarlos en firebase
    -webservice para registrar todo
    -talvez poner todo en wizards

--- app/src/main/java/com/carpooluniversitario/carpooluniversitario/MiFirebaseMessagingService.java
+++ app/src/main/java/com/carpooluniversitario/carpooluniversitario/MiFirebaseMessagingService.java
@@ -1,5 +1,12 @@
 package com.carpooluniversitario.carpooluniversitario;
 
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.media.RingtoneManager;
+import android.net.Uri;
+import android.support.v4.app.NotificationCompat;
 import android.util.Log;
 
 import com.google.firebase.messaging.FirebaseMessagingService;
@@ -15,7 +22,34 @@ private final String TAG = "NOTIFICATION X2";
 
         if (remoteMessage.getNotification() != null){
             Log.d(TAG, "Push Notificacion: "+remoteMessage.getNotification().getBody());
+            mostrarNotificacion(remoteMessage.getNotification().getTitle(),remoteMessage.getNotification().getBody());
         }
+
+        if (remoteMessage.getData().size()> 0){
+            Log.d(TAG, "onMessageReceived: "+remoteMessage.getData());
+        }
+
+
+
+    }
+
+    private void mostrarNotificacion(String title, String body) {
+        Intent intent = new Intent(this,MainActivity.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        PendingIntent pendingIntent= PendingIntent.getActivity(this,0,intent,PendingIntent.FLAG_ONE_SHOT);
+        Uri soundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+
+        NotificationCompat.Builder builder= new NotificationCompat.Builder(this)
+                .setSmallIcon(R.drawable.carpool_white_logo)
+                .setContentTitle(title)
+                .setContentText(body)
+                .setAutoCancel(true)
+                .setSound(soundUri)
+                .setColor(getResources().getColor(R.color.colorAccent))
+                .setContentIntent(pendingIntent);
+
+        NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        notificationManager.notify(0,builder.build());
 
     }
 }
